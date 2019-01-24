65d4f64106e5935e71a7a0254791b515c77c16cf
Parth Patel <parthpatel317@gmail.com>
2019-01-24 09:56:51 -0800

    add app folder with components, utils, and main.js

---
+++ app/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
