82f75eebd2ec4a78ffd0f2dcd302e8669ab9caa1
Parth Patel <parthpatel317@gmail.com>
2019-01-24 13:58:40 -0800

    add input component file. create text input feature in main.js

--- app/Main.js
+++ app/Main.js
@@ -3,16 +3,29 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { LinearGradient } from 'expo';
 import { primaryGradientArray } from './utils/Colors';
 import Header from './components/Header';
+import Input from './components/Input';
 
 const headerTitle = 'To Do';
 
 export default class Main extends React.Component {
+  state = {
+    inputValue: ''
+  };
+  newInputValue = value => {
+    this.setState({
+      inputValue: value
+    });
+  };
   render() {
+    const { inputValue } = this.state;
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
         <StatusBar barStyle="light-content" />
         <View style={styles.centered}>
           <Header title={headerTitle} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Input inputValue={inputValue} onChangeText={this.newInputValue} />
         </View>
       </LinearGradient>
     );
@@ -25,5 +38,9 @@ const styles = StyleSheet.create({
   },
   centered: {
     alignItems: 'center'
+  },
+  inputContainer: {
+    marginTop: 40,
+    paddingLeft: 15,
   },
 });
